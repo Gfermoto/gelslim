6dfeebb5c78083869a60707c7d21d6406998af75
Stanislav Kolesnik <gfermoto@gmail.com>
2024-08-21 13:02:39 +0300

    Update run_ros.sh

--- Software/Raspberry Pi/run_ros.sh
+++ Software/Raspberry Pi/run_ros.sh
@@ -8,18 +8,20 @@ xhost +local:docker
 DISPLAY_VAR=${DISPLAY}
 
 # Run the ROS Melodic container with correct display forwarding
-docker run -it --rm --privileged \
+docker run -it --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /dev/dri:/dev/dri \
-    -v $(pwd):/ws \
+    -v $(pwd):/sensor \
     --env="DISPLAY=$DISPLAY_VAR" \
     --env="QT_X11_NO_MITSHM=1" \
     -e USER=sensor \
     -e USER_UID=1000 \
     -e USER_GID=1000 \
+    --net=host \
     --ipc=host \
+    --name=ros1 \
     tiryoh/ros-melodic-desktop
 
 # Update and source the ROS environment in bashrc
-#echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
