ba7c9996f250a74aec0990ff980e9c493a4e4408
Stanislav Kolesnik <gfermoto@gmail.com>
2024-08-21 10:59:02 +0300

    Add files via upload

---
+++ Software/Raspberry Pi/run_ros.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Allow local connections from Docker containers to the X server
+export DISPLAY=:0.0
+xhost +local:docker
+
+# Get the DISPLAY variable from the host environment
+DISPLAY_VAR=${DISPLAY}
+
+# Run the ROS Melodic container with correct display forwarding
+docker run -it --rm --privileged \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /dev/dri:/dev/dri \
+    -v $(pwd):/ws \
+    --env="DISPLAY=$DISPLAY_VAR" \
+    --env="QT_X11_NO_MITSHM=1" \
+    -e USER=sensor \
+    -e USER_UID=1000 \
+    -e USER_GID=1000 \
+    --ipc=host \
+    tiryoh/ros-melodic-desktop
+
+# Update and source the ROS environment in bashrc
+#echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
